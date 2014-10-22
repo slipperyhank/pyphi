8efb011b3b18ff93f810b50a97f88a1ff0378899
Will Mayner <wmayner@gmail.com>
2014-10-22 17:27:56 -0500

    Add custom JSON encoder

--- pyphi/json.py
+++ pyphi/json.py
@@ -8,8 +8,46 @@ JSON
 PyPhi- and NumPy-aware JSON codec.
 """
 
+from collections import Iterable
 import numpy as np
 import json as _json
+
+from . import __version__
+
+
+def get_stamp(obj):
+    """Returns a dictionary with the key 'pyphi', containing the object's class
+    name and current PyPhi version."""
+    return {
+        'pyphi': {
+            'class': type(obj).__name__,
+            'version': __version__
+        }
+    }
+
+
+def make_encodable(obj):
+    """Return a JSON-encodable representation of an object, recursively using
+    any available ``json_dict`` methods, and NumPy's ``tolist`` function for
+    arrays."""
+    # Use the ``json_dict`` method if available, stamping it with the class
+    # name and the current PyPhi version.
+    if hasattr(obj, 'json_dict'):
+        d = obj.json_dict()
+        # Stamp it!
+        # TODO stamp?
+        # d.update(get_stamp(obj))
+        return d
+    # If we have an array, convert it to a list.
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    # If we have an iterable, recurse on the items. But not for strings, which
+    # are reCURSED! (they will recurse forver).
+    elif isinstance(obj, Iterable) and not isinstance(obj, str):
+        return [make_encodable(item) for item in obj]
+    # Otherwise, just return it.
+    else:
+        return obj
 
 
 class JSONEncoder(_json.JSONEncoder):
@@ -18,25 +56,16 @@ class JSONEncoder(_json.JSONEncoder):
     An extension of the built-in JSONEncoder that can handle native PyPhi
     objects as well as NumPy arrays.
 
-    Uses the ``json_dict`` method for PyPhi objects, and NumPy's ``tolist``
-    function for arrays.
+    Uses the ``json_dict`` method for PyPhi objects.
     """
 
-    def default(self, obj):
-        # If we have an array, convert it to a list.
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        # Otherwise, use the ``json_dict`` function if available.
-        try:
-            return obj.json_dict()
-        # If not, let the super handle it.
-        except AttributeError:
-            return super().default(obj)
-
     def encode(self, obj):
-        # Force ``default`` to be called even for already-serializable native
-        # Python types.
-        return super().encode(self.default(obj))
+        """Encode using the object's ``json_dict`` method if exists, falling
+        back on the built-in encoder if not."""
+        try:
+            return super().encode(obj.json_dict())
+        except AttributeError:
+            return super().encode(obj)
 
 
 def dumps(obj):
@@ -57,4 +86,4 @@ class JSONDecoder(_json.JSONDecoder):
 def loads(s):
     """Deserialize ``s`` (a ``str`` instance containing a JSON document) to a
     PyPhi object."""
-    pass
+    return _json.loads(s)
