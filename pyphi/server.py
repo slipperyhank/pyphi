d7344f7b684bb5c8323e361240727e81113eb442
Will Mayner <wmayner@gmail.com>
2014-10-23 10:50:01 -0500

    Clean up server

--- pyphi/server.py
+++ pyphi/server.py
@@ -10,23 +10,13 @@ Handles JSON-RPC calls to the remote API.
 
 from werkzeug.wrappers import Request, Response
 from werkzeug.serving import run_simple
-
 from jsonrpc import JSONRPCResponseManager
 
 from .remote_api import dispatcher
 
 
-@dispatcher.add_method
-def foobar(**kwargs):
-    return kwargs["foo"] + kwargs["bar"]
-
-
 @Request.application
 def application(request):
-    # Dispatcher is dictionary {<method_name>: callable}
-    dispatcher["echo"] = lambda s: s
-    dispatcher["add"] = lambda a, b: a + b
-
     response = JSONRPCResponseManager.handle(
         request.data, dispatcher)
     return Response(response.json, mimetype='application/json')
