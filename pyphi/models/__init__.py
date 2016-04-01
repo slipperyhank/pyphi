eea6ee26f1f1e2b3893f03b2f1cb66cdd3e657c7
William Marshall <wmarshall3@wisc.edu>
2016-04-01 14:31:37 -0500

    Add models and formatting for actual causation classes

--- pyphi/models/__init__.py
+++ pyphi/models/__init__.py
@@ -12,8 +12,12 @@ Attributes:
     Constellation: Alias for :class:`concept.Constellation`
     Cut: Alias for :class:`cuts.Cut`
     Part: Alias for :class:`cuts.Part`
+    AcMip: Alias for :class:`actual_causation.AcMip`
+    AcMice: Alias for :class:`actual_causation.AcMice`
+    AcBigMip: Alias for :class:`actual_causation.AcBigMip`
 """
 
 from .big_phi import BigMip, _null_bigmip, _single_node_bigmip
 from .concept import Mip, _null_mip, Mice, Concept, Constellation
 from .cuts import Cut, Part
+from .actual_causation import AcBigMip, AcMice, AcMip
