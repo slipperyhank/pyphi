4b6c71d8ef77f50650ca91c193ffa16a047cfd38
William Marshall <wmarshall3@wisc.edu>
2016-04-01 14:35:49 -0500

    Add Actual_Cut class to models

--- pyphi/models/__init__.py
+++ pyphi/models/__init__.py
@@ -12,6 +12,7 @@ Attributes:
     Constellation: Alias for :class:`concept.Constellation`
     Cut: Alias for :class:`cuts.Cut`
     Part: Alias for :class:`cuts.Part`
+    Actual_Cut: Alias for :class:`cuts.Actual_Cut`
     AcMip: Alias for :class:`actual_causation.AcMip`
     AcMice: Alias for :class:`actual_causation.AcMice`
     AcBigMip: Alias for :class:`actual_causation.AcBigMip`
@@ -19,5 +20,5 @@ Attributes:
 
 from .big_phi import BigMip, _null_bigmip, _single_node_bigmip
 from .concept import Mip, _null_mip, Mice, Concept, Constellation
-from .cuts import Cut, Part
+from .cuts import Cut, Part, Actual_Cut
 from .actual_causation import AcBigMip, AcMice, AcMip
