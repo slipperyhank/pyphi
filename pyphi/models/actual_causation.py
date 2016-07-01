3ed266ac10c9f967bab157b55ee2088b47e25c10
Larissa Albantakis <albantakis@wisc.edu>
2016-07-01 14:26:04 -0500

    updated cause and effect coefficient normalization
    - coefficients can now take partitions, which however is not really
    needed for actual causation calculations
    - the normalization was updated which changed the irreducible
    coefficient calculation, too
    - the module AcMip was updated

--- pyphi/models/actual_causation.py
+++ pyphi/models/actual_causation.py
@@ -113,10 +113,9 @@ def _general_eq(a, b, attributes):
 # Todo: Why do we even need this?
 # Todo: add second state
 _acmip_attributes = ['alpha', 'state', 'direction', 'mechanism', 'purview',
-                     'partition', 'probability', 'partitioned_probability',
-                     'unconstrained_probability']
+                     'partition', 'coefficient', 'partitioned_coefficient']
 _acmip_attributes_for_eq = ['alpha', 'direction', 'mechanism',
-                            'unpartitioned_ap']
+                            'coefficient']
 
 
 class AcMip(namedtuple('AcMip', _acmip_attributes)):
@@ -145,12 +144,10 @@ class AcMip(namedtuple('AcMip', _acmip_attributes)):
         partition (tuple(Part, Part)):
             The partition that makes the least difference to the mechanism's
             repertoire.
-        probability (float):
+        coefficient (float):
             The probability of the state in the past/future.
-        partitioned_probability (float):
+        partitioned_coefficient (float):
             The probability of the state in the partitioned repertoire.
-        unconstrained_probability (float):
-            The unconstrained probability of the state, used for normalization.
     """
     __slots__ = ()
 
@@ -238,7 +235,7 @@ class AcMice:
     def alpha(self):
         """
         ``float`` -- The difference between the mechanism's unpartitioned and
-        partitioned actual probabilities.
+        partitioned coefficient.
         """
         return self._mip.alpha
 
